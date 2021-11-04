b9647c4c12a5e4aa754063be6d0a2d0f0dcdc3fa
danschw <danschw@iu.edu>
2021-11-03 22:20:42 -0400

    align to BFG

--- vogdb/code/F_align_to_hmm_BFG.R
+++ vogdb/code/F_align_to_hmm_BFG.R
@@ -27,7 +27,7 @@ write.fasta(sequences = seq_select$seq,
             open = "w")  
 
 
-# > get sigBFG sigma profile -------------------------------------------
+# get sigBFG sigma profile -------------------------------------------
 
 tigr.sigma <- read_csv( here("TIGR/data","tigr_info_sigma.csv"))
 
@@ -39,22 +39,22 @@ sig_bfg.AC <- tigr.sigma %>%
 #path to HMM profile
 sig_bfg.hmm <- 
   str_subset(list.files(here("TIGR/data","tigr_hmm"), full.names = T),
-             sigf.AC) 
+             sig_bfg.AC) 
 # copy hmm profile to wd
-file.copy(sig_bfg.hmm, here("vogdb/data/hmm_align"))
+file.copy(from = sig_bfg.hmm, to = here("vogdb/data/hmm_align"), overwrite = T)
 
 sig_bfg.hmm <- 
   str_subset(list.files(here("TIGR/data","tigr_hmm")),
              sig_bfg.AC) 
 
-# > align to hmm ----------------------------------------------  
+# align to hmm ----------------------------------------------  
 setwd(here("vogdb/data/hmm_align"))
 
 # full alignment
 
 wsl <- paste(" wsl cat ", 
              "viral_sporulation_sigmas.faa",
-             "bact_sigF.faa  bact_sigG.faa|", # Bacillus and Clostr. sigF
+             "bsu_sigFG.faa|", # Bacillus subtilis. sigF + sigG
              "hmmalign --amino --informat FASTA",
              "-o SporeSigma_X_sigBFGProfile_full.aln",
              sig_bfg.hmm, "-")
@@ -63,9 +63,9 @@ system(wsl)
 
 wsl <- paste(" wsl cat ", 
              "viral_sporulation_sigmas.faa",
-             "bact_sigF.faa  bact_sigG.faa|", # Bacillus and Clostr. sigF
+             "bsu_sigFG.faa|", # Bacillus subtilis. sigF + sigG
              "hmmalign --amino --informat FASTA --trim",
-             "-o SporeSigma_X_sigfProfile_trim.aln",
+             "-o SporeSigma_X_sigBFGProfile_trim.aln",
              sig_bfg.hmm, "-")
 system(wsl)
 
@@ -78,7 +78,7 @@ for(i in 1:nrow(protein.names)){
 }
 writeLines(x1,"NAMES_SporeSigma_X_sigBFGProfile_full.aln" )
 
-# ___________________----
+# Perccent ID ----
 
 # pid
 pid_app <- "~/hmmer-3.3.1/easel/miniapps/esl-alipid"
@@ -86,24 +86,24 @@ pid_app <- "~/hmmer-3.3.1/easel/miniapps/esl-alipid"
 wsl <- paste(" wsl",
              pid_app,
              "--amino",
-             "SporeSigma_X_sigfProfile_trim.aln > pid_sigBFG.tsv")
+             "SporeSigma_X_sigBFGProfile_trim.aln > pid_sigBFG.tsv")
 
 system(wsl)
 
 
 
-# > parse results -----------------------------------------------------------
+# parse results -----------------------------------------------------------
 
 
-pid_sigF <- read.table(here("vogdb/data/hmm_align", "pid_sigF.tsv")) %>% 
+pid_sigBFG <- read.table(here("vogdb/data/hmm_align", "pid_sigBFG.tsv")) %>% 
   as_tibble()
-colnames(pid_sigF) <- c("seqname1","seqname2","perc_id","nid","denomid","perc_match","nmatch","denommatch")
+colnames(pid_sigBFG) <- c("seqname1","seqname2","perc_id","nid","denomid","perc_match","nmatch","denommatch")
 
 # names of proteins
 protein.names <- read_csv(here("vogdb/data/hmm_align","seq_names.csv"))
 
 # replace names
-pid_sigF <- left_join(pid_sigF, protein.names, by = c("seqname1" = "protein")) %>% 
+pid_sigBFG <- left_join(pid_sigBFG, protein.names, by = c("seqname1" = "protein")) %>% 
   mutate(seqname1 = name) %>% 
   rename(phage1 = phage) %>% 
   select(-name) %>% 
@@ -115,198 +115,23 @@ pid_sigF <- left_join(pid_sigF, protein.names, by = c("seqname1" = "protein")) %
 
 #organize data: phage vs host
 
-# keep only phage-host comparisons
-pid_sigF <- pid_sigF %>% 
-  filter(phage1 != phage2)
+# remove phage-phage comparisons
+pid_sigBFG <- pid_sigBFG %>% 
+  filter(! (phage2 & phage1))
 
-#__-----------
-# Repeat with sigF --------------------------------------------------------
-
-# > get sporulation sigma profile -------------------------------------------
-
-tigr.sigma <- read_csv( here("TIGR/data","tigr_info_sigma.csv"))
-
-# accession of HMM profile
-sigf.AC <- tigr.sigma %>% 
-  filter(str_detect(ID, regex("sigf", ignore_case = T))) %>% 
-  pull(AC)
-
-#path to HMM profile
-sigf.hmm <- 
-  str_subset(list.files(here("TIGR/data","tigr_hmm"), full.names = T),
-                       sigf.AC) 
-# copy hmm profile to wd
-file.copy(sigf.hmm, here("vogdb/data/hmm_align"))
-
-sigf.hmm <- 
-  str_subset(list.files(here("TIGR/data","tigr_hmm")),
-             sigf.AC) 
-
-# > align to hmm ----------------------------------------------  
-setwd(here("vogdb/data/hmm_align"))
-
-# full alignment
-
-wsl <- paste(" wsl cat ", 
-             "viral_sporulation_sigmas.faa",
-             "bact_sigF.faa |", # Bacillus and Clostr. sigF
-             "hmmalign --amino --informat FASTA",
-             "-o SporeSigma_X_sigfProfile_full.aln",
-             sigf.hmm, "-")
-system(wsl)
-
-
-wsl <- paste(" wsl cat ", 
-             "viral_sporulation_sigmas.faa",
-             "bact_sigF.faa |", # Bacillus and Clostr. sigF
-             "hmmalign --amino --informat FASTA --trim",
-             "-o SporeSigma_X_sigfProfile_trim.aln",
-             sigf.hmm, "-")
-system(wsl)
-
-
-# pid
-pid_app <- "~/hmmer-3.3.1/easel/miniapps/esl-alipid"
-
-wsl <- paste(" wsl",
-             pid_app,
-             "--amino",
-             "SporeSigma_X_sigfProfile_trim.aln > pid_sigF.tsv")
-
-system(wsl)
-
-
-# > parse results -----------------------------------------------------------
-
-
-pid_sigF <- read.table(here("vogdb/data/hmm_align", "pid_sigF.tsv")) %>% 
-  as_tibble()
-colnames(pid_sigF) <- c("seqname1","seqname2","perc_id","nid","denomid","perc_match","nmatch","denommatch")
-
-# names of proteins
-protein.names <- read_csv(here("vogdb/data/hmm_align","seq_names.csv"))
-
-# replace names
-pid_sigF <- left_join(pid_sigF, protein.names, by = c("seqname1" = "protein")) %>% 
-  mutate(seqname1 = name) %>% 
-  rename(phage1 = phage) %>% 
-  select(-name) %>% 
-  left_join(., protein.names, by = c("seqname2" = "protein")) %>% 
-  mutate(seqname2 = name) %>% 
-  rename(phage2 = phage) %>% 
-  select(-name)
-
-
-#organize data: phage vs host
-
-# keep only phage-host comparisons
-pid_sigF <- pid_sigF %>% 
-  filter(phage1 != phage2)
-
-
-
-#__-----------
-# Repeat with sigG --------------------------------------------------------
-
-
-# > get sporulation sigma profile -------------------------------------------
-
-# accession of HMM profile
-sigg.AC <- tigr.sigma %>% 
-  filter(str_detect(ID, regex("sigg", ignore_case = T))) %>% 
-  pull(AC)
-
-#path to HMM profile
-sigg.hmm <- 
-  str_subset(list.files(here("TIGR/data","tigr_hmm"), full.names = T),
-             sigg.AC) 
-# copy hmm profile to wd
-file.copy(sigg.hmm, here("vogdb/data/hmm_align"))
-
-sigg.hmm <- 
-  str_subset(list.files(here("TIGR/data","tigr_hmm")),
-             sigg.AC) 
-
-# > align to hmm ----------------------------------------------  
-setwd(here("vogdb/data/hmm_align"))
-
-# full alignment
-
-wsl <- paste(" wsl cat ", 
-             "viral_sporulation_sigmas.faa",
-             "bact_sigG.faa |", # Bacillus and Clostr. sigG
-             "hmmalign --amino --informat FASTA",
-             "-o SporeSigma_X_sigGProfile_full.aln",
-             sigg.hmm, "-")
-system(wsl)
-
-
-wsl <- paste(" wsl cat ", 
-             "viral_sporulation_sigmas.faa",
-             "bact_sigG.faa |", # Bacillus and Clostr. sigG
-             "hmmalign --amino --informat FASTA --trim",
-             "-o SporeSigma_X_sigGProfile_trim.aln",
-             sigg.hmm, "-")
-system(wsl)
-
-
-# pid
-pid_app <- "~/hmmer-3.3.1/easel/miniapps/esl-alipid"
-
-wsl <- paste(" wsl",
-             pid_app,
-             "--amino",
-             "SporeSigma_X_sigGProfile_trim.aln > pid_sigG.tsv")
-
-system(wsl)
-
-
-# > parse results -----------------------------------------------------------
-
-
-pid_sigG <- read.table(here("vogdb/data/hmm_align", "pid_sigG.tsv")) %>% 
-  as_tibble()
-colnames(pid_sigG) <- c("seqname1","seqname2","perc_id","nid","denomid","perc_match","nmatch","denommatch")
-
-# names of proteins
-protein.names <- read_csv(here("vogdb/data/hmm_align","seq_names.csv"))
-
-# replace names
-pid_sigG <- left_join(pid_sigG, protein.names, by = c("seqname1" = "protein")) %>% 
-  mutate(seqname1 = name) %>% 
-  rename(phage1 = phage) %>% 
-  select(-name) %>% 
-  left_join(., protein.names, by = c("seqname2" = "protein")) %>% 
-  mutate(seqname2 = name) %>% 
-  rename(phage2 = phage) %>% 
-  select(-name)
-
-
-#organize data: phage vs host
-
-# keep only phage-host comparisons
-pid_sigG <- pid_sigG %>% 
-  filter(phage1 != phage2)
-
-
-# plot --------------------------------------------------------------------
-
-
-pid <- bind_rows(pid_sigF %>% mutate(host_gene = "sigF"),
-                 pid_sigG %>% mutate(host_gene = "sigG"))
-
-
-p <- pid%>% 
-  mutate(seqname1 = str_remove(seqname1, "_.*")) %>% 
+# Plot --------------
+p <- pid_sigBFG%>% 
+  # mutate(perc_id = paste0(perc_id,"%")) %>% 
   ggplot(aes(seqname1, seqname2))+
-  geom_tile(aes(fill = perc_id))+
-  geom_text(aes(label = perc_id))+
+  geom_tile(aes(fill = perc_id), color = "white")+
+  geom_text(aes(label = paste0(perc_id,"%")))+
   scale_fill_viridis_c()+
-  facet_wrap(~host_gene, scales = "free_x")+
-  ylab("Phage")+
-  xlab("Host")+
+  ylab(NULL)+
+  xlab(NULL)+
   theme_classic()+
   panel_border(color = "black")
 
-save_plot(here("vogdb/figures","percID_align2profile.png"),p)
+save_plot(here("vogdb/figures","percID_align2profile.png"),p,
+          base_height = 2,base_width = 4)
+
 
